97f54e8db29a95c6af111efa2435a961ce00de4b
sebahernandez <sebaprogramer@gmail.com>
2024-08-12 22:41:05 -0400

    mejoras hero y integracion pricing

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'primary': '#075593',
-				'secondary': '#029EDC',
-			},
-		},
-		container: {
-			center: true,
-			padding: {
-				DEFAULT: '1rem',
-				sm: '2rem',
-				lg: '4rem',
-				xl: '5rem',
-			},
-		}
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2563EB",
+        secondary: "#111827",
+        accent: "#FEDF76",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+      },
+    },
+  },
+  plugins: [],
+};
